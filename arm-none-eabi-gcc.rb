026f28c19dec2ac5ed9ecc4bfc0019d4c989f0c7
Tim Zhang <mythfish@gmail.com>
2016-02-03 22:37:19 +0800

    Update arm-none-eabi-gcc.rb
    Fix sha

--- arm-none-eabi-gcc.rb
+++ arm-none-eabi-gcc.rb
@@ -3,7 +3,7 @@ class ArmNoneEabiGcc < Formula
   homepage "https://launchpad.net/gcc-arm-embedded"
   url "https://launchpad.net/gcc-arm-embedded/5.0/5-2015-q4-major/+download/gcc-arm-none-eabi-5_2-2015q4-20151219-mac.tar.bz2"
   version "20160203"
-  sha256 "a6353db31face60c2091c2c84c902fc4d566decd1aa04884cd822c383d13c9fa"
+  sha256 "41056ffeba4bcb5bbea13185461a1269613ac13321fbda3e7dc59ee664ee3f06"
 
   def install
     cp_r ["arm-none-eabi", "bin", "lib", "share"], "#{prefix}/"
